f7f9000a3fe47b893d31ed5786185abc33f3f83f
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-07 15:23:49 +0100

    First

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+    return (        
+        <p>Je suis le footer. Tu me vois ? Je suis là ! En bas !</p>        
+    )
+} 
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <div>
+        <h1>Mon super Header qui déchire</h1>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Home</li>                
+                <li>Contact</li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
+
+export default Header;
