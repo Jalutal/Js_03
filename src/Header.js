1de1c0d8f837c81f0dbceefb8a8cca60bb6259c1
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-08 10:38:50 +0100

    Gestion panier

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,26 @@
 function Header() {
     const userLogged = true /* ou false, variable pour tester l'user */
+    const itemsInCart = 2 /* ou 0 pour tester la variable des items dans le panier */ 
     return (
         <header>
         <h1>Mon super Header qui déchire</h1>
         <nav>
             <ul>
-                <li>Accueil</li>
-                <li>Home</li>                
-                <li>Contact</li>               
-                { userLogged ? 
+            { userLogged ? 
                     (<li>Bonjour User !</li>)
                 :
                     (<li>Bonjour, connecte toi !</li>)
                 }
+                <li>Accueil</li>
+                <li>Home</li>                
+                <li>Contact</li>  
+
+                {itemsInCart > 0 ?
+                    (<li>Vous avez {itemsInCart} objets dans votre panier</li>)
+                :
+                    (<li>Votre panier est vide</li>)
+                }             
+                
             </ul>
         </nav>
         </header>
